0f79b51a3fddb3442fa3a9fb8d27c7f22f992c80
Longura1124 <Longurashvilinikoloz@gmail.com>
2026-05-26 00:42:52 +0400

    add auto db creation

--- app.py
+++ app.py
@@ -11,3 +11,9 @@ db = SQLAlchemy(app)
 
 # IMPORTANT: import routes AFTER app + db
 from routes import *
+
+# ADD THIS AT THE BOTTOM:
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # This builds your Game and User tables in Render automatically
+    app.run(debug=True)
