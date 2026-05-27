14ba7d863f2e19a18457a4a2b7e80af42a343c76
Longura1124 <Longurashvilinikoloz@gmail.com>
2026-05-27 13:26:23 +0400

    fix app.py

--- app.py
+++ app.py
@@ -1,17 +1,1 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-from routes import *
-
-# ADD THIS AT THE BOTTOM:
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+from ext import app
